44764657d656859ddb89c14247fd0135fb2d2280
beveratraining <79100134+beveratraining@users.noreply.github.com>
2021-10-25 00:30:23 -0400

    Add files via upload

---
+++ dataframe_patient.R
@@ -0,0 +1,9 @@
+patients <- data.frame(Student_Id = c(1012301, 1012302, 1012303,1012304, 1012305),
+                      Firstname = c('John', 'Jeff', 'Ronald', 'Jennifer', 'Jessica'),
+                      Lastname = c('Novak', 'Barr', 'Lum', 'Forbis', 'Connor'),
+                      Subject_Id = c('SAE6A', 'SAE6B', 'SAE6C', 'SAE6G', 'SAE61'),
+                      Age = c(20, 19, 20, 19, 20),
+                      Sex = c('M', 'M', 'M', 'F', 'F'))
+print(patients)
+
+#using functions from the guide, explore the "patient" dataframe
